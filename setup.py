8ae684a41ce903f9ef61da8a5b4c06324026addc
Anthony So <anthonyjso@users.noreply.github.com>
2016-09-09 02:21:25 -0400

    Specify sqlparse==0.1.19 in setup.py for bug
    http://stackoverflow.com/questions/38479063/django-debug-toolbar-breaking-on-admin-while-getting-sql-stats
    https://sqlparse.readthedocs.io/en/latest/intro/
    https://django-debug-toolbar.readthedocs.io/en/stable/changes.html?highlight=sqlparse#id1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(exclude=('tests.*', 'tests', 'example')),
     install_requires=[
         'django>=1.4.2',
-        'sqlparse',
+        'sqlparse==0.1.19'
     ],
     include_package_data=True,
     zip_safe=False,                 # because we're including static files
