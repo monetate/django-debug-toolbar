4056f11b07134b251b5ab2ef36810febbfaf5d3e
blingblin-g <luvbliny@gmail.com>
2025-04-10 01:38:27 +0900

    Add show_toolbar_with_docker function for Docker IP handling
    - Introduced a new function, show_toolbar_with_docker, to determine if the toolbar should be displayed when running inside Docker containers.
    - Updated installation documentation to reference the new function for Docker configurations.
    - Added a changelog entry for the new functionality.
    
    Co-authored-by: Matthias Kestenholz <mk@feinheit.ch>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -15,7 +15,11 @@ from django.test import AsyncRequestFactory, RequestFactory
 from django.test.utils import override_settings
 
 from debug_toolbar.forms import SignedDataForm
-from debug_toolbar.middleware import DebugToolbarMiddleware, show_toolbar
+from debug_toolbar.middleware import (
+    DebugToolbarMiddleware,
+    show_toolbar,
+    show_toolbar_with_docker,
+)
 from debug_toolbar.panels import Panel
 from debug_toolbar.toolbar import DebugToolbar
 
@@ -72,7 +76,8 @@ class DebugToolbarTestCase(BaseTestCase):
         with self.settings(INTERNAL_IPS=[]):
             # Is true because REMOTE_ADDR is 127.0.0.1 and the 255
             # is shifted to be 1.
-            self.assertTrue(show_toolbar(self.request))
+            self.assertFalse(show_toolbar(self.request))
+            self.assertTrue(show_toolbar_with_docker(self.request))
         mocked_gethostbyname.assert_called_once_with("host.docker.internal")
 
     def test_not_iterating_over_INTERNAL_IPS(self):
